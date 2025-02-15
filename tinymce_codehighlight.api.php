af4a14057bc63d7f7f473e9042f2374bf1147695
indigoxela <user@olga.indigofloat.at>
2025-02-15 14:19:42 +0100

    Improve api file

--- tinymce_codehighlight.api.php
+++ tinymce_codehighlight.api.php
@@ -7,16 +7,17 @@
 /**
  * Implements hook_tinymce_codehighlight_theme_info().
  *
- * Provide a custom theme to display code snippets. You only need some CSS.
+ * Provide a custom theme to display code snippets the way you like it.
  */
 function hook_tinymce_codehighlight_theme_info() {
   $path = backdrop_get_path('module', 'mymodule');
-  $themes['default'] = array(
+  $themes['my_own'] = array(
     'name' => 'my_own',
     'label' => t('My Own'),
+    // Have a look at the existing themes - only some lines of CSS.
     'stylesheet_path' => $path . '/themes/myown/styles.css',
-    // Optional:
-    //'screenshot_path' => $path . '/themes/myown/screenshot.png',
+    // Optional: a small preview image. Can be omitted.
+    'screenshot_path' => $path . '/themes/myown/screenshot.png',
   );
 
   return $themes;
